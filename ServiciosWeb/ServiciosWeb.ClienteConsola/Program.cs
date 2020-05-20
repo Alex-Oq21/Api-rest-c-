585d04fc6fbbffd2e5345f2ad64bcf983bf12ad9
Alexis Cárdenas <alexiscardednas85@hotmail.com>
2020-05-20 18:42:05 -0500

    Se cosnume el servicio REST desde una aplicación de consola.

--- ServiciosWeb/ServiciosWeb.ClienteConsola/Program.cs
+++ ServiciosWeb/ServiciosWeb.ClienteConsola/Program.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ServiciosWeb.ClienteConsola
 {
@@ -12,11 +14,26 @@ namespace ServiciosWeb.ClienteConsola
         static void Main(string[] args)
         {
             //Invoca servicio ASMX
-          ServicioLibroSoapClient cliente = new ServicioLibroSoapClient();
-           var libros = cliente.ObtenerLibros();
+          //ServicioLibroSoapClient cliente = new ServicioLibroSoapClient();
+          // var libros = cliente.ObtenerLibros();
+          //  //Invocar servicio WCF
+          //  ServiceLibroWCF.Service1Client clienteWCF = new ServiceLibroWCF.Service1Client();
+          //  var libros2 = clienteWCF.ObtenerLibros();
 
-            ServiceLibroWCF.Service1Client clienteWCF = new ServiceLibroWCF.Service1Client();
-            var libros2 = clienteWCF.ObtenerLibros();
+            //Invocar servicio REST
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:60702/");
+            var request = clienteHttp.GetAsync("api/Libro").Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var listado = JsonConvert.DeserializeObject<List<Libro>>(resultString);
+                foreach (var item in listado)
+                {
+                    Console.WriteLine(item.Titulo);
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
