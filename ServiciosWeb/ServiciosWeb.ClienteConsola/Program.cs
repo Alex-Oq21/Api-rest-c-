be6b06ea1e0487e9003fb3ffc8a982eda5c5dc5a
Alexis Cárdenas <alexiscardednas85@hotmail.com>
2020-05-20 17:31:14 -0500

    Se implemento Servicio WCF y ASMX.

---
+++ ServiciosWeb/ServiciosWeb.ClienteConsola/Program.cs
@@ -0,0 +1,22 @@
+﻿using ServiciosWeb.ClienteConsola.ServiceLibroASMX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiciosWeb.ClienteConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Invoca servicio ASMX
+          ServicioLibroSoapClient cliente = new ServicioLibroSoapClient();
+           var libros = cliente.ObtenerLibros();
+
+            ServiceLibroWCF.Service1Client clienteWCF = new ServiceLibroWCF.Service1Client();
+            var libros2 = clienteWCF.ObtenerLibros();
+        }
+    }
+}
