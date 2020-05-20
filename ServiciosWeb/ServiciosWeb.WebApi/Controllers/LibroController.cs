b0dfcf95c255577044abb7f232748cd3f237e005
Alexis Cárdenas <alexiscardednas85@hotmail.com>
2020-05-19 19:03:30 -0500

    Se implemento servicios REST para consultar todos los los libros de la base de datos, como el de un único libro, más la referencia de ldatos a la parte de la lógica.

---
+++ ServiciosWeb/ServiciosWeb.WebApi/Controllers/LibroController.cs
@@ -0,0 +1,27 @@
+﻿using ServiciosWeb.Datos.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiciosWeb.WebApi.Controllers
+{
+    public class LibroController : ApiController
+    {
+        LibreriaConnection BD = new LibreriaConnection();
+        [HttpGet]
+        public IEnumerable<Libro> Get()
+        {
+            var listado = BD.Libro.ToList();
+            return listado;
+        }
+        [HttpGet]
+        public Libro Get(int id)
+        {
+            var libro = BD.Libro.FirstOrDefault(x=> x.IdLibro ==id);
+            return libro;
+        }
+    }
+}
