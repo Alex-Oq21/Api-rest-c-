7eb649134dbcf3b2cc5e97773c4fd89d1859db31
Alexis Cárdenas <alexiscardednas85@hotmail.com>
2020-05-22 20:43:08 -0500

    Se agregaron métodos Post, Put, Delete para realizar las tareas Crud por medio de APIS.

--- ServiciosWeb/ServiciosWeb.WebApi/Controllers/LibroController.cs
+++ ServiciosWeb/ServiciosWeb.WebApi/Controllers/LibroController.cs
@@ -23,5 +23,33 @@ namespace ServiciosWeb.WebApi.Controllers
             var libro = BD.Libro.FirstOrDefault(x=> x.IdLibro ==id);
             return libro;
         }
+        [HttpPost]
+        public  bool Post(Libro libro)
+        {
+            BD.Libro.Add(libro);
+            return BD.SaveChanges() >0;
+        }
+        [HttpPut]
+        public bool Put(Libro libro)
+        {
+            var libroactulizar = BD.Libro.FirstOrDefault(x => x.IdLibro == libro.IdLibro);
+            libroactulizar.Titulo = libro.Titulo;
+            libroactulizar.Sipnosis = libro.Sipnosis;
+            libroactulizar.Autor = libro.Autor;
+            libroactulizar.Editorial = libro.Editorial;
+            libroactulizar.Idioma = libro.Idioma;
+            libroactulizar.Formato = libro.Formato;
+            libroactulizar.Genero = libro.Genero;
+            libroactulizar.Disponible = libro.Disponible;
+            return BD.SaveChanges() > 0;
+        }
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            var libroEliminar = BD.Libro.FirstOrDefault(x => x.IdLibro == id);
+            BD.Libro.Remove(libroEliminar);
+            return BD.SaveChanges() > 0;
+
+        }
     }
 }
