2d9954d9c4ec80f29432ccb745e1dab42b3b8f7b
Alexis Cárdenas <alexiscardednas85@hotmail.com>
2020-05-25 18:12:37 -0500

    Se implemento CRUD por medio de APIS.

--- ServiciosWeb/ServiciosWeb.ClienteWeb/Controllers/LibroController.cs
+++ ServiciosWeb/ServiciosWeb.ClienteWeb/Controllers/LibroController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http.Formatting;
 
 namespace ServiciosWeb.ClienteWeb.Controllers
 {
@@ -25,6 +26,94 @@ namespace ServiciosWeb.ClienteWeb.Controllers
                 return View(listado);
             }
             return View(new List<Libro>());
+        }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(Libro libro)
+        {
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:60702/");
+            var request = clienteHttp.PostAsync("api/Libro", libro, new JsonMediaTypeFormatter()).Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var correcto = JsonConvert.DeserializeObject<bool>(resultString);
+                if (correcto)
+                {
+                    return RedirectToAction("index");
+                }
+                return View(libro);
+            }
+            return View(libro);
+        }
+
+        [HttpGet]
+        public ActionResult Actualizar(int id)
+        {
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:60702/");
+            var request = clienteHttp.GetAsync("api/Libro?id=" + id).Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var informacion = JsonConvert.DeserializeObject<Libro>(resultString);
+                return View(informacion);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Actualizar(Libro libro)
+        {
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:60702/");
+            var request = clienteHttp.PutAsync("api/Libro", libro, new JsonMediaTypeFormatter()).Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var correcto = JsonConvert.DeserializeObject<bool>(resultString);
+                if (correcto)
+                {
+                    return RedirectToAction("index");
+                }
+                return View(libro);
+            }
+            return View(libro);
+        }
+        [HttpGet]
+        public ActionResult Eliminar(int id)
+        {
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:60702/");
+            var request = clienteHttp.DeleteAsync("api/Libro?id=" + id).Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var correcto = JsonConvert.DeserializeObject<bool>(resultString);
+                if (correcto)
+                {
+                    return RedirectToAction("index");
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Detalle(int id)
+        {
+            HttpClient clienteHttp = new HttpClient();
+            clienteHttp.BaseAddress = new Uri("http://localhost:60702/");
+            var request = clienteHttp.GetAsync("api/Libro?id=" + id).Result;
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                var informacion = JsonConvert.DeserializeObject<Libro>(resultString);
+                return View(informacion);
+            }
+            return View();
         }
     }
 }
